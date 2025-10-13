f9df0232f87fcab233bcee1ae56d3c24933ecf7c
Roma Fatkudinov ИП-513 <roma1903fat@gmail.com>
2025-10-13 23:47:55 +0700

    ex5

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    
+    const int m = 10;
+    const int n = 3;
+    const int q = 10;
+
+    int m1[m][n];
+    int m2[n][q];
+    int m3[m][q];
+ 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &m1[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < q; j++) {
+            scanf("%d", &m2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < q; j++) {
+            m3[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                m3[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < q; j++) {
+            printf("%d ", m3[i][j]);
+        }
+    }
     return 0;
 }
