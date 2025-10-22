44f4aee51845b077cb5e1ebbf87787556dd17587
Роман Фаткудинов ИП-513 <roma1903fat@gmail.com>
2025-10-22 10:52:15 +0300

    ex1

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -1,7 +1,39 @@
 #include "../include/advanced_array_function.h"
 
 int max_subarray_sum(int* nums, int size) {
-    return 0;
+    int max_sum = -99999999;
+    int cur_sum;
+
+    if (size == 0) {
+        return 0;
+    }
+
+    if (size == 1) {
+        max_sum = nums[0];
+    }
+
+    if (size > 1) {
+        for (int i = 0; i < size; i++) {
+            cur_sum = nums[i];
+
+            for (int j = i; j < size; j++) {
+                if (nums[i] != nums[j]) {
+                    cur_sum += nums[j];
+                }
+                if (cur_sum > max_sum) {
+                  max_sum = cur_sum;
+                }
+            }
+
+            if (cur_sum > max_sum) {
+                max_sum = cur_sum;
+            }
+            
+            cur_sum = 0;
+        }
+    }
+
+    return max_sum;
 }
 
 int length_of_lis(int* nums, int numsSize) {
